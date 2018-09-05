fb15c8488e6670e6eb2ece7a8d176ad1489af3c4
Daniel Gordon <daniel.ben.gordon@gmail.com>
2018-09-05 16:48:54 -0400

    Added the file, "newfile1.c"

---
+++ newfile1.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newfile1.c
+ * Author: Danny G
+ *
+ * Created on September 5, 2018, 4:47 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
