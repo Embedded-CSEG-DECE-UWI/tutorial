34df194c7ac4afc85953eb7b30ba3aee6511d85b
Daniel Gladstone <daniel.gladstone@gmail.com>
2018-09-05 19:50:36 -0400

    This is the LED tutorial Home-Work. Testing has not been performed yet.

---
+++ ledblink_DRG_F.c
@@ -0,0 +1,25 @@
+#include <p18cxxx.h>            //includes library to use features of pic18
+#include <delays.h>             //includes library to use delay functions
+
+/*Configuration pragmas for the c18 C compiler*/
+#pragma config FOSC = HS        
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+
+typedef int bool;               //defining a new data type with additional keywords
+#define true 1
+#define false 0;
+
+void main(void){
+    
+    bool isWorking = true;      //initializes the variable to true
+    
+    TRISB = 0;                  //configures all the TRISB bits as outputs
+    
+    while(isWorking){           //while this condition is the same, the block of code is repeated
+        PORTB = 1;              //sets PORTB to high (this turns the LED on)
+        Delay10KTCYx(100);      //delays for 1 sec
+        PORTB = 0;              //sets PORTB to low (this turns the LED off)
+    }
+
+}
