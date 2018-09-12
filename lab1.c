83ef900828388dbe12945b19bbc4298ca102e59c
CathyRadix <Cathy.Radix@sta.uwi.edu>
2018-09-12 10:24:08 -0400

    Create lab1.c

---
+++ lab1.c
@@ -0,0 +1,12 @@
+/* Lab 1 first file - ID number xxxxxx Group xxx  */
+#include <p18f452.h>   
+
+/* Set configuration bits for use with PICKit3 and 4MHz oscillator */
+#pragma config OSC = HS
+#pragma config WDT = OFF
+#pragma config LVP = OFF
+
+void main (void)
+{
+    /* Insert code from pre-lab Q14 to change values on PORTB */
+}
