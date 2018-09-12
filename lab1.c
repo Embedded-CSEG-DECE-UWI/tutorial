554a59799b688ae5ddb7eb261158bfbc85a7056b
Amritta <42347332+Amritta@users.noreply.github.com>
2018-09-12 16:55:52 -0400

    testing

--- lab1.c
+++ lab1.c
@@ -1,12 +1,19 @@
 /* Lab 1 first file - ID number 815006394 Group A   */
-#include <p18f452.h>   
+#include <p18cxxx.h>   
 
 /* Set configuration bits for use with PICKit3 and 4MHz oscillator */
 #pragma config OSC = HS
 #pragma config WDT = OFF
 #pragma config LVP = OFF
 
+int count;
 void main (void)
 {
     /* Insert code from pre-lab Q14 to change values on PORTB */
+    count = 1;
+    TRISB = 0;      //config PORTB as output
+    for(count = 1; count<=15; count++)
+    {
+        PORTB = count;  //displays count
+    }
 }
