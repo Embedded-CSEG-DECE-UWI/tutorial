dd1eb2bab9e6553b4526f2ad617e651e84fea3e6
Daniel Gordon <danny.b.gordon@gmail.com>
2018-09-12 21:06:56 -0400

    Small changes

--- ledBlinkFunc_Ver1.1_DBG_F.c
+++ ledBlinkFunc_Ver1.1_DBG_F.c
@@ -1,22 +1,39 @@
 #include <p18cxxx.h>            //includes library to use features of pic18
 #include <delays.h>             //includes library to use delay functions
-
+#include <timers.h>             //includes library to use timer functions    
 
 /*Configuration pragmas for the c18 C compiler*/
 #pragma config OSC = XT         //Oscillator set to external
 #pragma config WDT = OFF        //Watchdog Timer OFF
 #pragma config LVP = OFF        //Low Voltage Programming OFF
 
-void toggleBit();               //Prototype needed for the 'goto' below
+int cnt = 0;
+void low_isr();
+void toggleBit();
 
-#pragma code HIGH_INTERRUPT_VECTOR = 0x8
+#pragma code low_vector = 0x18
 void high_ISR()
 {
-    goto toggleBit();
+    _asm
+      GOTO low_isr
+    _endasm
 }
 #pragma code                    //Allows the linker to locate the remaining code
 
-#pragma interrupt toggleBit
+#pragma interruptlow low_isr
+void low_isr()
+{
+    if(INTCONbits.TMR0IF == 1)
+    {
+        if(cnt == 100)
+        {
+            cnt = 0;
+            toggleBit();
+            WriteTimer0(25535);
+        }
+        INTCONbits.TMR0IF = 0;
+    }
+}
 //Toggles the state of the bit`
 void toggleBit()
 {
@@ -25,10 +42,20 @@ void toggleBit()
 }
 
 void configTimer()
-{}
+{
+  //Turn off timer0
+  //Set timer to 16 bit mode
+  //Set to timer source to internal
+  //Clear PSA bit
+  //Set prescalar to 2
+  //Load Timer0
+  //Turn on Timer0
+  //Set the Timer0 Interrupt Enable Bit
+  //Enable Peripheral Interrupt
+  //Enable Global Interrupt Enable bit
+}
 
-void configGlobalInterrupts()
-{}
+
 
 
 void main(void)
